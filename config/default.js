4ca4fc6845938e85d53e6bd4a064a0338d404e00
guofangchao <guofangchao@daling.com>
2018-11-02 18:21:47 +0800

    add mysql

--- config/default.js
+++ config/default.js
@@ -18,5 +18,30 @@ module.exports = {
     /**
      * rabbitmq的连接地址
      */
-    amqp: "amqp://admin:112233@39.105.125.190"
+    amqp: "amqp://admin:112233@39.105.125.190",
+    /**
+     * 数据库设置，参考sequelize
+     */
+    dbconfig: {
+        database: "test",
+        usr: "root",
+        pwd: "123456",
+        cfg: {
+            dialect: "mysql",
+            host: "127.0.0.1",
+            pool: {
+                max: 5,
+                min: 0,
+                idle: 10000
+            }
+        }
+    },
+    /**
+     * redis的配置，参考redis
+     */
+    redis: {
+        port: 6379,
+        host: "127.0.0.1",
+        pwd: "123456"
+    }
 }
